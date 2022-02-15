13622a4ac60afd274862b4c70324d8a5f0d674ad
Varsion <jianhual12@outlook.com>
2022-02-15 18:16:11 +0800

    add auth actions

---
+++ src/actions/auth/Auth.ts
@@ -0,0 +1,25 @@
+import { gql } from '@apollo/client'
+
+export const SIGN_IN = gql`
+  mutation SignIn($email: String!, $password: String!) {
+    signIn(email: $email, password: $password) {
+      token
+      user {
+        id
+        email
+        name
+      }
+    }
+  }`;
+
+export const SIGN_UP = gql`
+  mutation SignUp($email: String!, $password: String!, $name: String!) {
+    signUp(email: $email, password: $password, name: $name) {
+      token
+      user {
+        id
+        email
+        name
+      }
+    }
+  }`;

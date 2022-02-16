0caaf8df08bf16516b6599d965b5d036ee3f87de
Varsion <jianhual12@outlook.com>
2022-02-16 22:44:27 +0800

    refactor env file

--- src/apollo/ApolloClient.ts
+++ src/apollo/ApolloClient.ts
@@ -1,7 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { BaseUrl } from "@src/env";
 
 export const client = new ApolloClient({
-  uri: BaseUrl,
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });

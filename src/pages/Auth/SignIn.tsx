4f657ae23188abf13bef5f23accc3aa69da7a5c9
Varsion <jianhual12@outlook.com>
2022-04-23 15:09:57 +0800

    feat: sign in #2

--- src/pages/Auth/SignIn.tsx
+++ src/pages/Auth/SignIn.tsx
@@ -1,52 +1,34 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import FormGroup from '@mui/material/FormGroup';
-import Link from '@mui/material/Link';
+import { Link, Box, Container, Typography, CardContent, Card } from '@mui/material'
+
+import SignInForm from '../../components/SignInForm';
 
 import ParticlesBg from 'particles-bg';
 import '../../styles/app.css';
 import logo from "../../assets/logo.png"
 
-const sign_in = (
-  <CardContent>
-    <Box style={{
-      display: 'flex',
-      justifyContent: 'center',
-    }}>
-      <img src={logo} alt="logo" style={{maxWidth:200}}/>
-    </Box>
-    <Typography variant="h5" component="div">
-      Sign In
-    </Typography>
-
-    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        Don't have a account? <Link href="/#/sign_up" variant="body2">{'sign up now'}</Link>
-    </Typography>
-    <FormGroup>
-      <TextField fullWidth label="Email" id="email" margin="normal" required/>
-      <TextField fullWidth label="Password" id="password" margin="normal" type="password" required/>
-
-      <Box style={{
-        display: 'flex',
-        justifyContent: 'center',
-      }}>
-        <Button variant="contained">Sign In</Button>
-      </Box>
-    </FormGroup>
-  </CardContent>
-);
-
 export default function SignIn() {
   return (
     <Container maxWidth="md">
       <ParticlesBg type="circle" bg={true} />
-      <Card className='center'>{sign_in}</Card>
+      <Card className='center'>
+        <CardContent>
+          <Box style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}>
+            <img src={logo} alt="logo" style={{maxWidth:200}}/>
+          </Box>
+          <Typography variant="h5" component="div">
+            Sign In
+          </Typography>
+
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              Don't have a account? <Link href="/#/sign_up" variant="body2">{'sign up now'}</Link>
+          </Typography>
+          <SignInForm />
+        </CardContent>
+      </Card>
     </Container>
   );
 }

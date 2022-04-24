e38dd9c1698cfd839662fdc179321c82bc292bbc
Varsion <jianhual12@outlook.com>
2022-04-24 17:11:26 +0800

    feat: add authorization

--- src/apollo/ApolloClient.ts
+++ src/apollo/ApolloClient.ts
@@ -1,6 +1,23 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-export const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
 });
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+export default client;

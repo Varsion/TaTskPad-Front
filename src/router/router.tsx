703b44fd184b7bf2b3129f90a854477e8fa57dbe
handou2 <2864466519@qq.com>
2022-04-26 20:46:01 +0800

    feat: add hello

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,16 +1,11 @@
-import React, { useEffect } from 'react'
-import {
-  HashRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import SignIn from '../pages/Auth/SignIn';
-import SignUp from '../pages/Auth/SignUp';
-import Dashboard from '../pages/Dashboard/Dashboard';
-import Project from '../pages/Dashboard/Project';
-import Organization from '../pages/Organization/Organization';
-import Kanban from '../pages/Kanban/Kanban';
-
+import React, { useEffect } from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import SignIn from "../pages/Auth/SignIn";
+import SignUp from "../pages/Auth/SignUp";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import Project from "../pages/Dashboard/Project";
+import Organization from "../pages/Organization/Organization";
+import Kanban from "../pages/Kanban/Kanban";
 
 export default function Router() {
   return (
@@ -23,7 +18,8 @@ export default function Router() {
 
         <Route path="/sign_in" element={<SignIn />} />
         <Route path="/sign_up" element={<SignUp />} />
+        {/* Hello */}
       </Routes>
     </HashRouter>
-  )
+  );
 }

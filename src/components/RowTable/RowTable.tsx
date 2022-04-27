fb2e2cb638ce605dbfe3a741823db02fe6802fb1
handou2 <2864466519@qq.com>
2022-04-27 15:02:58 +0800

    fix;Backlog-MinTable

--- src/components/RowTable/RowTable.tsx
+++ src/components/RowTable/RowTable.tsx
@@ -25,20 +25,9 @@ import MinTable from "./MinTable";
 //   },
 // });
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-  price: number
-) {
+function createData(name: string, price: number) {
   return {
     name,
-    calories,
-    fat,
-    carbs,
-    protein,
     price,
     history: [
       { date: "2020-01-05", customerId: "11091700" },
@@ -67,10 +56,6 @@ function Row(props: { row: ReturnType<typeof createData> }) {
         <TableCell component="th" scope="row">
           {row.name}
         </TableCell>
-        <TableCell align="right">{row.calories}</TableCell>
-        <TableCell align="right">{row.fat}</TableCell>
-        <TableCell align="right">{row.carbs}</TableCell>
-        <TableCell align="right">{row.protein}</TableCell>
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
@@ -95,11 +80,8 @@ function Row(props: { row: ReturnType<typeof createData> }) {
 }
 
 const rows = [
-  createData("To-do list", 159, 6.0, 24, 4.0, 3.99),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3, 4.99),
-  //   createData("Eclair", 262, 16.0, 24, 6.0, 3.79),
-  //   createData("Cupcake", 305, 3.7, 67, 4.3, 2.5),
-  //   createData("Gingerbread", 356, 16.0, 49, 3.9, 1.5),
+  createData("To-do list", 159),
+  createData("Ice cream sandwich", 237),
 ];
 
 export default function CollapsibleTable() {

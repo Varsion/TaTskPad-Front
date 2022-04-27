3a035ac345bc1804ff276b7af74a7b2fc3af1ed1
Varsion <jianhual12@outlook.com>
2022-04-27 20:09:22 +0800

    feat: update issue page

---
+++ src/components/Issue/IssueStatusCard.tsx
@@ -0,0 +1,72 @@
+import React from "react"
+import { LoadingButton } from "@mui/lab"
+import { Card, CardContent, Grid, FormControl, InputLabel, Select, MenuItem, TextField } from "@mui/material"
+
+import { SelectChangeEvent } from "@mui/material/Select";
+
+const IssueStatusCard = () => {
+
+  const [priority, setPriority] = React.useState('p1');
+  const handlePriorityChange = (event: SelectChangeEvent) => {
+    setPriority(event.target.value as string);
+  };
+
+  return (
+    <Card sx={{ minWidth: 300 }}>
+      <CardContent>
+        <Grid container direction="column" alignItems="stretch" spacing={2}>
+          <Grid item>
+            <FormControl variant="standard" fullWidth>
+              <InputLabel id="demo-simple-select-label">Priority</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={priority}
+                label="Age"
+                onChange={handlePriorityChange}
+              >
+                <MenuItem value={'p0'}>P0</MenuItem>
+                <MenuItem value={'p1'}>P1</MenuItem>
+                <MenuItem value={'p2'}>P2</MenuItem>
+                <MenuItem value={'p3'}>P3</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item>
+            <FormControl variant="standard" fullWidth>
+              <InputLabel id="demo-simple-select-label">Type</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={'story'}
+                label="Type"
+                onChange={handlePriorityChange}
+              >
+                <MenuItem value={'story'}>Story</MenuItem>
+                <MenuItem value={'bug'}>Bug</MenuItem>
+                <MenuItem value={'task'}>Task</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <TextField id="standard-basic" label="Estimate" variant="standard" value={'1d'} fullWidth/>
+          </Grid>
+          <Grid item style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}>
+            <LoadingButton 
+              loading={false}
+              variant="contained" 
+              type='submit' >
+                Update
+            </LoadingButton>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default IssueStatusCard
--- src/pages/Kanban/IssuePage.tsx
+++ src/pages/Kanban/IssuePage.tsx
@@ -1,29 +1,19 @@
 import React from "react";
-import ShareIcon from "@mui/icons-material/Share";
 import {
-  Box,
-  Toolbar,
-  Breadcrumbs,
-  Link,
-  TextField,
-  Grid,
-  Button,
-  Typography,
-  Stack,
-  InputLabel,
-  Tab,
-  Collapse,
-  MenuItem,
+  Box, Toolbar, Breadcrumbs, Link, Grid, 
+  Typography, InputLabel, MenuItem,
 } from "@mui/material";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
-import DoneAllIcon from "@mui/icons-material/DoneAll";
+import IssueStatusCard from "../../components/Issue/IssueStatusCard";
+
 export default function IssuePage() {
   const [age, setAge] = React.useState("");
 
   const handleChange = (event: SelectChangeEvent) => {
     setAge(event.target.value as string);
   };
+
   return (
     <Box sx={{ flexGrow: 1, p: 3 }}>
       <Toolbar />
@@ -51,39 +41,39 @@ export default function IssuePage() {
               Add daily payout schedule
             </Typography>
             <h4>描述</h4>
-            <Box>
+            <Typography variant="body1" gutterBottom>
               Add "daily" payout schedule option in payout account setting
-            </Box>
-            <Box
-              style={{
-                display: "flex",
-                marginTop: "10px",
-                width: "800px",
-                height: "500px",
-              }}
-            >
-              <img src="https://source.unsplash.com/random" alt="descript" />
-            </Box>
+              Add "daily" payout schedule option in payout account setting
+              Add "daily" payout schedule option in payout account setting
+              Add "daily" payout schedule option in payout account setting
+              Add "daily" payout schedule option in payout account setting
+              Add "daily" payout schedule option in payout account setting
+            </Typography>
           </Grid>
-          <Grid item xs={3}>
-            <Box sx={{ minWidth: 120 }}>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={age}
-                  label="Age"
-                  onChange={handleChange}
-                >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
-            </Box>
+          <Grid item xs={4}>
+            <Grid container direction="column" alignItems="stretch">
+              <Grid item>
+                <Box sx={{ maxWidth: 300 }}>
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={age}
+                      label="Age"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={10}>Ten</MenuItem>
+                      <MenuItem value={20}>Twenty</MenuItem>
+                      <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+                <Toolbar />
+                <IssueStatusCard />
+              </Grid>
+            </Grid>
           </Grid>
-          {/* Header */}
         </Grid>
       </Box>
     </Box>

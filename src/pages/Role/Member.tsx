bd7b1a51e41f915dd9dc8b2ac64f6407b1a20ddb
Varsion <jianhual12@outlook.com>
2022-05-06 10:03:09 +0800

    feat: add the members faker date #1

--- src/pages/Role/Member.tsx
+++ src/pages/Role/Member.tsx
@@ -7,7 +7,23 @@ import {
 } from '@mui/material';
 import HeaderBar from "../../components/Items/HeaderBar";
 
+interface MembersType {
+  id: number;
+  name: string;
+  enterTime: string;
+  role: string;
+}
+
 const Members = () => {
+
+  const [members, setMembers] = React.useState<MembersType[]>([
+    {id: 1, name: 'jianhua', enterTime: '2022-05-01', role: 'Admin'},
+    {id: 2, name: 'Jianhua 01', enterTime: '2022-05-04', role: 'Developer'},
+    {id: 3, name: 'Jianhua 02', enterTime: '2022-05-04', role: 'Developer'},
+    {id: 4, name: 'Jianhua 03', enterTime: '2022-05-04', role: 'Developer'},
+  ]);
+
+
   return (
     <Container sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'column', mt:10 }}>
       <Toolbar />
@@ -33,85 +49,31 @@ const Members = () => {
           </TableHead>
           <TableBody>
 
-            <TableRow
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                Jianhua
-              </TableCell>
-              <TableCell component="th" scope="row">
-                2022-04-04
-              </TableCell>
-              <TableCell component="th" scope="row">
-                Admin
-              </TableCell>
-              <TableCell align="right" sx={{display: 'flex'}} >
-                <Stack spacing={2} direction="row">
-                  <Button variant="outlined" color="error">Disabled</Button>
-                  <Button variant="contained">Update</Button>
-                </Stack>
-              </TableCell>
-            </TableRow>
-
-            <TableRow
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                ChongYaaa
-              </TableCell>
-              <TableCell component="th" scope="row">
-                2022-04-04
-              </TableCell>
-              <TableCell component="th" scope="row">
-                Developer
-              </TableCell>
-              <TableCell align="right" sx={{display: 'flex'}} >
-                <Stack spacing={2} direction="row">
-                  <Button variant="outlined" color="error">Disabled</Button>
-                  <Button variant="contained">Update</Button>
-                </Stack>
-              </TableCell>
-            </TableRow>
-            
-            <TableRow
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                Varsion
-              </TableCell>
-              <TableCell component="th" scope="row">
-                2022-04-01
-              </TableCell>
-              <TableCell component="th" scope="row">
-                Developer
-              </TableCell>
-              <TableCell align="right" sx={{display: 'flex'}} >
-                <Stack spacing={2} direction="row">
-                  <Button variant="outlined" color="error">Disabled</Button>
-                  <Button variant="contained">Update</Button>
-                </Stack>
-              </TableCell>
-            </TableRow>
-
-            <TableRow
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                Nemo
-              </TableCell>
-              <TableCell component="th" scope="row">
-                2022-04-01
-              </TableCell>
-              <TableCell component="th" scope="row">
-                Developer
-              </TableCell>
-              <TableCell align="right" sx={{display: 'flex'}} >
-                <Stack spacing={2} direction="row">
-                  <Button variant="outlined" color="error">Disabled</Button>
-                  <Button variant="contained">Update</Button>
-                </Stack>
-              </TableCell>
-            </TableRow>
+            {
+              members.map((member, index) => {
+                return (
+                  <TableRow
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    key={index}
+                  >
+                    <TableCell component="th" scope="row">
+                      {member.name}
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                      {member.enterTime}
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                      {member.role}
+                    </TableCell>
+                    <TableCell align="right" sx={{display: 'flex'}} >
+                      <Stack spacing={2} direction="row">
+                        <Button variant="contained">Settings</Button>
+                      </Stack>
+                    </TableCell>
+                  </TableRow>
+                )
+              })
+            }
 
           </TableBody>
         </Table>
